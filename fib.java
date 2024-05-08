9c3b9781f01dbc41902b5d4c4168bdb5eff104fd
Shruti kolhe <86185288+SHRUTIKOLHE03@users.noreply.github.com>
2024-05-08 15:44:46 +0530

    Update fib.java

--- fib.java
+++ fib.java
@@ -6,7 +6,7 @@ class fib
   {   
       System.out.println("Fibonacci Series Code: ");
       int n1=0,n2=1,n3,i,count=10;    
-      System.out.println(n1+" "+n2);//printing 0 and 1    
+      System.out.print(n1+" "+n2);//printing 0 and 1    
       for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
       {    
           n3=n1+n2;    
